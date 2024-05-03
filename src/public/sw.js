315e346e9a7c65349b5d8f64fe2e86a3e98b7663
RayLight <arusurobo@gmail.com>
2024-05-04 00:22:17 +0900

    update service worker

--- src/public/sw.js
+++ src/public/sw.js
@@ -1,7 +1,6 @@
 const CACHE_NAME = "pwa-sample-caches-v1";
 // Cache targets
 const urlsToCache = [];
-
 self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
